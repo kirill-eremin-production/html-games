b5cd5ce5e6f856f99d8d0df3a6b574befd7da88c
Kirill Eremin <kirill.eremins@yandex.ru>
2026-02-23 17:53:19 +0300

    fix hud

--- src/modes/galaxy/index.ts
+++ src/modes/galaxy/index.ts
@@ -14,6 +14,7 @@ import {
 } from '@/campaign/galaxy-scene';
 import { addToScene } from '@/shared/core';
 import { playerPlane } from '@/systems/player';
+import { hideHUD } from '@/ui/hud';
 
 import type { GalaxyModeContext, GameModeHandler } from '../types';
 
@@ -41,6 +42,7 @@ export const galaxyMode: GameModeHandler = {
     const onExploration = context?.onExploration;
 
     ensureGalaxyBuilt();
+    hideHUD();
     playerPlane.visible = false;
 
     showGalaxy();
